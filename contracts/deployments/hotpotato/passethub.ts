d663bd0bec7de91f892abe418c533e91a21e64f9
Cherrypick14 <cherrypicklet@gmail.com>
2025-08-24 23:25:23 +0300

    feat: add hotpotato contract deployment artifacts

---
+++ contracts/deployments/hotpotato/passethub.ts
@@ -0,0 +1,5 @@
+// Deployment file for passethub chain
+// Contract deployed and active
+
+export const evmAddress = "0x012aA4Df3E1Dd88e3bb987902C773BDEf706aE09"
+export const ss58Address = "0x012aA4Df3E1Dd88e3bb987902C773BDEf706aE09" // Same as EVM address for this chain 
---
+++ contracts/deployments/hotpotato/pop.ts
@@ -0,0 +1,5 @@
+// Placeholder deployment file for pop chain
+// This will be populated after actual deployment
+
+export const evmAddress = "0x0000000000000000000000000000000000000000" // Placeholder
+export const ss58Address = "0x0000000000000000000000000000000000000000000000000000000000000000" // Placeholder 
